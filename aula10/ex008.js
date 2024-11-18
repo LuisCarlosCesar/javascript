159d0b1c782eaec4e1e917af31665a3c04f91925
Luis Carlos Cesar <luis.l2ve@gmail.com>
2024-11-18 13:11:22 -0300

    c

---
+++ aula10/ex008.js
@@ -0,0 +1,6 @@
+let vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){    
+console.log('Voce passou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de seguranca!')
---
+++ aula10/ex009.js
@@ -0,0 +1,8 @@
+let pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('voce é Brasileiro')
+}
+else{
+    console.log('Voce é Estrangeiro')
+}
